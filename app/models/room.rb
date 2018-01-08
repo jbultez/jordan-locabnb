6a4cee59abce43cffaeb0b0c4e429cd37baefbfd
j.bultez <jordan.bultez@gmail.com>
2018-01-08 15:29:38 +0100

    create review model

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,6 +2,7 @@ class Room < ApplicationRecord
   belongs_to :user
   has_many :photos
   has_many :reservations
+  has_many :reviews
 
 
   validates :home_type, presence: true
@@ -15,4 +16,8 @@ class Room < ApplicationRecord
 
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
+
+  def average_rating
+    reviews.count == 0 ? 0 : reviews.average(:star).round(2)
+  end
 end
